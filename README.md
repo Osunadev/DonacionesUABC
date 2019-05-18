880e30dac79740819d63235f096979d82d4bd182
Omar Osuna <omar.alonso.osuna.angulo@uabc.edu.mx>
2019-05-17 22:41:06 -0700

    App iniciada con la view de Menu principal completada y con la view de menu de usuario logged in en proceso

---
+++ README.md
@@ -0,0 +1,4 @@
+# Donaciones UABC
+> En esta aplicación vamos a utilizar Firebase en conjunto con Android Studio para darle una mínima funcionalidad de hacer donaciones y solicitar donaciones a la aplicación.
+
+El plan es hacer vistas de los mockups previamente realizados en moqups.com. Tenemos la ventaja de que los mockups que hicimos ya están enumerados, por lo que se le podrán asignar más facilmente a cada uno de nosotros.
---
+++ app/src/main/java/com/example/donacionesuabc/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.donacionesuabc;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnArticulos;
+    Button logInButton;
+    TextView registroTxt;
+    TextView titleApp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnArticulos = (Button) findViewById(R.id.verArticulosBtn);
+        logInButton = (Button) findViewById(R.id.loginBtn);
+        registroTxt = (TextView) findViewById(R.id.registroTxt);
+        titleApp = (TextView) findViewById(R.id.menuTitleTxt);
+
+
+    }
+
+    public void logInUser(View view) {
+        // Por ahorita este boton nos dirigira directamente a la pantalla de menu de usuario Logged In
+        Intent i1 = new Intent(this, MenuLoggedActivity.class);
+        startActivity(i1);
+    }
+}
