0acaf2a0953be0f7c5be465649a0bbeb52b6a441
Isaac <isaac.hernandez.cano@uabc.edu.mx>
2019-05-24 14:12:41 -0700

    view 1.2 finished

---
+++ app/src/main/java/com/example/donacionesuabc/CustomItems.java
@@ -0,0 +1,19 @@
+package com.example.donacionesuabc;
+
+public class CustomItems {
+    private String spinnerText;
+    private int spinnerImage;
+
+    public CustomItems(String spinnerText, int spinnerImage) {
+        this.spinnerText = spinnerText;
+        this.spinnerImage = spinnerImage;
+    }
+
+    public String getSpinnerText() {
+        return spinnerText;
+    }
+
+    public int getSpinnerImage() {
+        return spinnerImage;
+    }
+}
--- app/src/main/java/com/example/donacionesuabc/MainActivity.java
+++ app/src/main/java/com/example/donacionesuabc/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -32,5 +33,13 @@ public class MainActivity extends AppCompatActivity {
         // Por ahorita este boton nos dirigira directamente a la pantalla de menu de usuario Logged In
         Intent i1 = new Intent(this, MenuLoggedActivity.class);
         startActivity(i1);
+    }
+
+    public void menu(View view){
+        try {
+            startActivity(new Intent(this, VerArticulos.class));
+        }catch (Exception e){
+            Log.d("Error",e.toString());
+        }
     }
 }
